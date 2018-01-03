022e0b2f8a8260afc466d71fdc699a0206a5d718
yoheitaonishi <onishiyoheita@gmail.com>
2018-01-03 22:07:04 +0900

    added lenet

--- clone.py
+++ clone.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
-
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 import csv
 import cv2
 import numpy as np
@@ -15,7 +16,7 @@ with open('./data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
-del lines[0]
+del lines[0] # delete csv header
 
 for line in lines:
     source_path = line[0]
@@ -30,10 +31,43 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+'''
+pixel_normalized = pixel / 255
+pixel_mean_centered = pixel_normalized - 0.5
+'''
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+'''
+Convolution2D explain
+'''
+model.add(Convolution2D( 6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D( 6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+'''
+Why 120
+'''
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 model.save('model.h5')
+
+'''
+6428/6428 [==============================] - 304s - loss: 0.6888 - val_loss: 0.0141
+Epoch 2/7
+6428/6428 [==============================] - 244s - loss: 0.0119 - val_loss: 0.0126
+Epoch 3/7
+6428/6428 [==============================] - 243s - loss: 0.0106 - val_loss: 0.0116
+Epoch 4/7
+6428/6428 [==============================] - 846s - loss: 0.0097 - val_loss: 0.0114
+Epoch 5/7
+6428/6428 [==============================] - 243s - loss: 0.0089 - val_loss: 0.0109
+Epoch 6/7
+6428/6428 [==============================] - 243s - loss: 0.0080 - val_loss: 0.0110
+Epoch 7/7
+6428/6428 [==============================] - 1035s - loss: 0.0071 - val_loss: 0.0110
+'''
